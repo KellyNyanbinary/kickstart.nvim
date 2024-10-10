498ee4a433a18a13915f0c63e0ff3faa773c7729
Kelly Wu <43101126+KellyNyanbinary@users.noreply.github.com>
2024-10-10 14:10:36 -0600

    Turn off expand tab

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ vim.opt.scrolloff = 5
 -- vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 4
 -- vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
